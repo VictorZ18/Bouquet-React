e59ece588ae5ae64daf34945ef8f98221573c6bb
Victor Zehmour <victor.zehmour@anvil.be>
2024-06-18 13:35:49 +0200

    Small tweaks

--- bouquet/src/views/App.js
+++ bouquet/src/views/App.js
@@ -62,7 +62,7 @@ function Home() {
   const deadline = new Date(Date.parse(new Date()) + days * 24 * 60 * 60 * 1000);
 
   const closeRectangle = () => {
-    const rectangle = document.querySelector(".rectangle");
+    const rectangle = document.querySelector(".rectangleHome");
     const arrow = document.querySelector(".arrow-down");
     const title = document.querySelector(".titlePage");
     if (rectangle) {
@@ -82,7 +82,7 @@ function Home() {
         <WeddingHeader userName={user.user.firstName} />
       </header>
       <Clock countdown={deadline} />
-      <div className="rectangle lower">
+      <div className="rectangleHome lower">
         <img src={require("../icons/arrow-down.png")} alt="arrow-down" className="arrow-down arrow-up" onClick={closeRectangle} />
         <div className="wrapper pageBottom">
           <h1 className="titlePage titleMargin titleLowMargin">Activities</h1>
